68675d30cc3f2d554cd53c26c4530ee62d02d219
bravokeyl <bravokeyl@gmail.com>
2017-05-13 11:38:21 +0530

    working build

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 
 import NuevoApp from './app/index';
+import MyApp from './app/drawer';
 
 StatusBar.setBackgroundColor('#fa5e2b');
 AppRegistry.registerComponent('nuevo2', () => NuevoApp);
