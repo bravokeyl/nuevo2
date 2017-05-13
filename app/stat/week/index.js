ca947eb42fc556466c5f9e343812f00adf05c933
bravokeyl <bravokeyl@gmail.com>
2017-05-13 17:14:12 +0530

    Remove pathjs-charts

--- app/stat/week/index.js
+++ app/stat/week/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Image, ScrollView } from 'react-native';
 import { FormLabel, FormInput, Button, Icon, Card } from 'react-native-elements';
 import { NavigationActions } from 'react-navigation';
+import { Bar } from 'react-native-pathjs-charts'
 import NuevoFireAuth from '../../firebase';
 var user = NuevoFireAuth.currentUser;
 import NuevoStyles from './styles';
@@ -31,6 +32,75 @@ export default class NuevoStatWeek extends Component {
   }
 
   render(){
+    let data = [
+    [{
+      "v": 49,
+      "name": "apple"
+    }, {
+      "v": 42,
+      "name": "apple"
+    }],
+    [{
+      "v": 69,
+      "name": "banana"
+    }, {
+      "v": 62,
+      "name": "banana"
+    }],
+    [{
+      "v": 29,
+      "name": "grape"
+    }, {
+      "v": 15,
+      "name": "grape"
+    }]
+  ]
+
+  let options = {
+    width: 300,
+    height: 300,
+    margin: {
+      top: 20,
+      left: 25,
+      bottom: 50,
+      right: 20
+    },
+    color: '#2980B9',
+    gutter: 20,
+    animate: {
+      type: 'oneByOne',
+      duration: 200,
+      fillTransition: 3
+    },
+    axisX: {
+      showAxis: true,
+      showLines: true,
+      showLabels: true,
+      showTicks: true,
+      zeroAxis: false,
+      orient: 'bottom',
+      label: {
+        fontFamily: 'Arial',
+        fontSize: 8,
+        fontWeight: true,
+        fill: '#34495E'
+      }
+    },
+    axisY: {
+      showAxis: true,
+      showLines: true,
+      showLabels: true,
+      showTicks: true,
+      zeroAxis: false,
+      orient: 'left',
+      label: {
+        fontFamily: 'Arial',
+        fontSize: 8,
+        fontWeight: true,
+        fill: '#34495E'
+      }
+    }
+  }
     return(
       <ScrollView style={ [NuevoStyles.container] }>
         <View style={ [NuevoStyles.container] }>
@@ -43,9 +113,7 @@ export default class NuevoStatWeek extends Component {
                 <Text style={[NuevoStyles.val]}>52,190</Text>
               </View>
               <View style={[NuevoStyles.right]}>
-                <Text style={[NuevoStyles.key]}>
-                 The idea with React Native Elements is more about component structure than actual design.
-                </Text>
+                <Bar data={data} options={options} accessorKey='v'/>
               </View>
             </View>
           </Card>
