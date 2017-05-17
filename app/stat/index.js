33318a0b0aa5b28dd52f44d2c81c60b6ccc19fc4
bravokeyl <bravokeyl@gmail.com>
2017-05-17 17:26:11 +0530

    install charts react-native-pathjs-charts

--- app/stat/index.js
+++ app/stat/index.js
@@ -54,11 +54,6 @@ export default class NuevoStat extends Component {
     return(
       <ScrollView style={ [NuevoStyles.container] }>
         <View style={ [NuevoStyles.container] }>
-          <Card>
-            <Text style={{marginBottom: 10}}>
-             The idea with React Native Elements is more about component structure than actual design.
-            </Text>
-          </Card>
           <View style={[NuevoStyles.datcon]}>
               {
                 this.state.data.map((l, i) => (
